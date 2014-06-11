85f86b5aef60d34f14e4cc3d48b283e714ca0f08
Karin <mixmixkarin@hotmail.com>
2014-06-11 10:07:17 +0700

    MySimpleApp

---
+++ src/mysimpleapp/MySimpleApp.java
@@ -0,0 +1,20 @@
+/*
+
+ */
+
+package mysimpleapp;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MySimpleApp {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+    
+}
